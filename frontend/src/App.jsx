06b42944ab23ff76231d73861ca91c6d91a22b4d
Haru <sumitcoding7@gmail.com>
2024-03-22 14:36:19 +0530

    created routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,27 @@
 import { useState } from 'react'
 import './App.css'
+import { BrowserRouter , Routes , Route} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Signin from './pages/Signin'
+import Profile from './pages/Profile'
+import SignUp from './pages/SignUp'
+
 
 function App() {
 
 
   return (
-    <>
-      App
-    </>
+    <BrowserRouter >
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+
+      </Routes>
+    </BrowserRouter >
   )
 }
 
