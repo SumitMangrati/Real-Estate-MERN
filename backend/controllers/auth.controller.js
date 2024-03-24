6711f36fd5de23a1e9dabbd8f5d54eef382977a8
Haru <sumitcoding7@gmail.com>
2024-03-24 13:52:13 +0530

    signin functionality

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,8 @@
+import mongoose from "mongoose";
 import User from "../models/user.model.js";
 import bcrypt from 'bcrypt';
+import { errorHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req , res , next) => {
     const { username , email , password } = req.body;
@@ -12,4 +15,33 @@ export const signup = async (req , res , next) => {
     catch(err){
         next(err);
     }
+};
+
+export const signin = async (req , res , next) => {
+    const { email , password } = req.body;
+    try{
+        const validUser = await User.findOne({ email});
+        if(!validUser){
+            return next(errorHandler(404 , 'User not found')); 
+        }
+        const isValidPassword = bcrypt.compareSync(password , validUser.password);
+        if(!isValidPassword){
+            return next(errorHandler(401 , 'Invalid password'));
+        }
+
+        //creating jwt token
+        const token = jwt.sign({ id: validUser._id} , process.env.JWT_SECRET );
+
+        //destructuring password from user object
+        const { password:pass , ...rest } = validUser._doc;
+        res
+        .cookie('access_token' , token , { httpOnly: true })
+        .status(200)
+        .json(rest);
+
+    }
+    catch(err){
+        next(err);
+    }
+    
 }
