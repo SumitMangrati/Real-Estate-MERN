6b909526b696dac11170f406459add7589860896
Haru <sumitcoding7@gmail.com>
2024-03-24 14:24:39 +0530

    added redux

--- frontend/src/pages/Signin.jsx
+++ frontend/src/pages/Signin.jsx
@@ -2,11 +2,18 @@
 import React from "react";
 import { useState } from "react";
 import { Link , useNavigate } from "react-router-dom";
+import { useDispatch , useSelector } from "react-redux";
+import { signInStart , signInFail , signInSuccess } from "../redux/user/userSlice";
+
+
+
 function Signin() {
   const [formData, setFormData] = useState({});
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const { error, loading } = useSelector((state) => state.user);
+//   const [error, setError] = useState(null);
+//   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const handleChange = (e) => {
     setFormData({
@@ -17,7 +24,8 @@ function Signin() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try{
-        setLoading(true);
+        //setLoading(true);
+        dispatch(signInStart());
         const res = await fetch('/api/auth/signin' ,{
             method: 'POST' ,
             headers: {
@@ -27,17 +35,20 @@ function Signin() {
         })
         const data = await res.json();
         if(data.success === false){
-            setError(data.message);
-            setLoading(false);
+            // setError(data.message);
+            // setLoading(false);
+            dispatch(signInFail(data.message));
             return;
         }
-        setLoading(false);
-        setError(null);
-        navigate('/');
+        // setLoading(false);
+        // setError(null);
+        dispatch(signInSuccess(data));
+         navigate('/');
     }
     catch(err){
-        setLoading(false);
-        setError('Something went wrong');
+        dispatch(signInFail(err.message));
+        // setLoading(false);
+        // setError('Something went wrong');
     }
   }
 
